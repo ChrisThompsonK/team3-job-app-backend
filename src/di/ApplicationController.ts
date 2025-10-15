2b83ebe37d6d086c5a8ac44001e35a6533f0a998
EmmaKainos <emma-louise.grier@kainos.com>
2025-10-15 11:49:06 +0100

    added small piece of validation for applicants

--- src/di/ApplicationController.ts
+++ src/di/ApplicationController.ts
@@ -1,9 +1,14 @@
 import { ApplicationController } from '../controllers/ApplicationController.js';
 import { ApplicationRepository } from '../repositories/ApplicationRepository.js';
+import { JobRepository } from '../repositories/JobRepository.js';
 import { ApplicationService } from '../services/ApplicationService.js';
 
 const applicationRepository: ApplicationRepository = new ApplicationRepository();
-const applicationService: ApplicationService = new ApplicationService(applicationRepository);
+const jobRepository: JobRepository = new JobRepository();
+const applicationService: ApplicationService = new ApplicationService(
+  applicationRepository,
+  jobRepository
+);
 const applicationController: ApplicationController = new ApplicationController(applicationService);
 
 export { applicationController };
